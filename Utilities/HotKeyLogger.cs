2fc1d2986e798b3ceba8074583f826f2c0b36ee8
bikester1 <cohen7424@hotmail.com>
2025-08-09 16:44:15 -0400

    Added comments to HotKeyLogger class

--- Utilities/HotKeyLogger.cs
+++ Utilities/HotKeyLogger.cs
@@ -11,7 +11,12 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace BikesExtraHotKey.Debugger
 {
-	public class HotKeyLogger
+    /// <summary>
+    /// Class <c>HotKeyLogger</c> a logging class used to allow initialization and queueing of logs prior to final logger and mod initialization.
+    /// <br/>
+    /// Ff the parent mod has not been initialized it will queue messages until the parent provides an instance of logger at which point it will flush queued messages.
+    /// </summary>
+    public class HotKeyLogger
 	{
         private ILog logger;
         public bool debugMod;
@@ -19,13 +24,27 @@ namespace BikesExtraHotKey.Debugger
 		private bool initialized = false;
 
 
-
-		public HotKeyLogger(bool debugMod = false)
+        /// <summary>
+        /// Constructor <c>HotKeyLogger</c> construct a logger without an instance of ILog.
+		/// <br/>
+		/// Constuction in this manner will be set to uninitialized and all messages will be queued until InitializeLogger is called and a valid instance of ILog is provided.
+		/// <br/>
+		/// Once InitializeLogger is called all queued messages will be flushed to the provided logger.
+        /// </summary>
+        /// <param name="debugMod"></param> If set to true additional call stack info is included with log messages to help with debugging.
+        public HotKeyLogger(bool debugMod = false)
 		{
 			this.debugMod = debugMod;
             initialized = false;
 		}
 
+		/// <summary>
+		/// Constructor <c>HotKeyLogger</c> constructs a logger instance with an instance of ILog log.
+		/// <br/>
+		/// Construction in this manner will always be initialized and will never utilize log queue instead meassages are immediately logged when log methods are called.
+		/// </summary>
+		/// <param name="log"></param> Provided ILog for performing logging.
+		/// <param name="debugMod"></param> If set to true additional call stack info is included with log messages to help with debugging.
         public HotKeyLogger(ILog log, bool debugMod = false)
 		{
 			logger = log;
@@ -33,13 +52,22 @@ namespace BikesExtraHotKey.Debugger
             initialized = true;
 		}
 
-		public void InitializeLogger(ILog log)
+        /// <summary>
+        /// Method <c>InitializeLogger</c> will assign an ILog instance, set the initialized state to true and flush the queue of messages to the provided logger.
+        /// </summary>
+        /// <param name="log"></param> Provided ILog for performing logging.
+        public void InitializeLogger(ILog log)
 		{
 			logger = log;
             initialized = true;
 			FlushQueue();
 		}
 
+		/// <summary>
+		/// Method FlushQueue flushes all messages to the assigned logger and should only be called after initialization has complete.
+		/// <br/>
+		/// This is not guarenteed to be infallable so is a private method and should be used with care.
+		/// </summary>
 		private void FlushQueue()
 		{
 			foreach ((LogLevel level, object logMessage) in LogQueue)
